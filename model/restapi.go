28b40c10a1fa58009de82f64029b3d797465a5d3
yusys-cloud <yangzq80@gmail.com>
2021-09-10 10:52:25 +0800

    doc(readme):Add total nums.

--- model/restapi.go
+++ model/restapi.go
@@ -4,13 +4,13 @@
 package model
 
 type Data struct {
-	Total int
-	Items interface{}
+	Total int         `json:"total"`
+	Items interface{} `json:"items"`
 }
 
 type Response struct {
-	Code int
-	Data *Data
+	Code int   `json:"code"`
+	Data *Data `json:"data"`
 }
 
 func NewResponse() *Response {
